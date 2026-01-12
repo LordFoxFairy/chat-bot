04579aa6a80628d71487d2c66100fc07d665fc62
Xiangyu <default@example.com>
2026-01-12 22:48:36 +0800

    fix: 删除不存在的 BaseHandler 导入

--- modules/__init__.py
+++ modules/__init__.py
@@ -2,9 +2,9 @@ from .base_vad import BaseVAD
 from .base_asr import BaseASR
 from .base_llm import BaseLLM
 from .base_tts import BaseTTS
-from .base_handler import BaseHandler
 from .base_module import BaseModule
+from .base_protocol import BaseProtocol
 
 __all__ = [
-    "BaseVAD", "BaseASR", "BaseLLM", "BaseTTS", "BaseHandler", "BaseModule"
+    "BaseVAD", "BaseASR", "BaseLLM", "BaseTTS", "BaseModule", "BaseProtocol"
 ]
